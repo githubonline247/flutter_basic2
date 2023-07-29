5a2bd835d2658dd12b10e6f0e335896bd0387ba4
MarkusDachs <93642381+MarkusDachs@users.noreply.github.com>
2023-07-29 15:46:36 +0530

    home screen file created

---
+++ lib/home_screen.dart
@@ -0,0 +1,21 @@
+
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({ Key? key }) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class StatefulWidget {
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
